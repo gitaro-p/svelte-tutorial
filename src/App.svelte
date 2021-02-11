00a1c6670476aed7b80416902b036372e441dc71
清川　裕太郎 <yuu7353@gmail.com>
2021-02-11 15:58:27 +0900

    first todo

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,61 @@
+<!-- HTML部 -->
+<div>
+  絞り込み:
+  <button on:click={() => { condition = null }}>すべて</button>
+  <button on:click={() => { condition = false }}>未完了</button>
+  <button on:click={() => { condition = true }}>完了</button>
+</div>
+<div>
+  <input type="text" bind:value={title} bind:this={initFocus}>
+  <button on:click={() => add()}>タスク追加</button>
+</div>
+<div>
+  <ul>
+    {#each filteredTodoList(todoList, condition)  as todo (todo.id)}
+      <li>
+        <input type="checkbox" bind:checked={todo.done}> {todo.title}
+      </li>
+    {/each}
+  </ul>
+</div>
+
 <script>
-	export let name;
+  import { onMount } from 'svelte'
+
+  let title = ''
+  let initFocus = null
+  let condition = null
+  let todoList = [
+    { id: 0, done: false, title: 'プログラミングを勉強する'},
+    { id: 1, done: false, title: 'サプライズ用の指輪を買う'},
+    { id: 2, done: false, title: 'フラッシュモブダンスを練習する'},
+  ]
+
+  onMount(() => {
+    init()
+  })
+
+  function init() {
+    title = ''
+    initFocus.focus()
+  }
+
+  function add() {
+    todoList = [...todoList,
+    {
+      id: todoList.length,
+      done: false,
+      title
+    }]
+    init()
+  }
+
+  $: filteredTodoList = (todoList, condition) => {
+    return condition === null ? todoList : todoList.filter(t => t.done === condition)
+  }
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
-
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  /* TODO: CSS得意なフレンズ絶賛募集中！！ */
+  /* https://svelte.dev/tutorial/styling */
 </style>
